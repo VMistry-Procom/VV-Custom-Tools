891c33c56e319e01bbeea3975f26182dfe670768
VMistry-Procom <103944476+VMistry-Procom@users.noreply.github.com>
2022-05-06 13:04:39 -0400

    Update VV5 Control Search.js

--- VV5 Control Search.js
+++ VV5 Control Search.js
@@ -2,28 +2,31 @@
 // @name         VV5 Control Search
 // @namespace    https://github.com/jsevilla-procom
 // @version      1.0
-// @description  Waits for elements to load then creates a search bar for groups and conditions filtering
+// @description  Waits for elements to load then creates a search bar for Controls filtering
 // @author       Vishul Mistry
 // @match        https://*.visualvault.com/FormDesigner/*
 // @grant        none
 // ==/UserScript==
 
-var InitSearchBar = function(controlsListElem) {
+var InitSearchBar = function(controlsListItemElem) {
     // create and place search bar into the HTML
     var searchBar = $('<input id="mySearch" style="display:block" type="text">');
-    searchBar.insertBefore(controlsListElem.parent().parent());
+    searchBar.insertBefore(controlsListItemElem.parent().parent());
 
-    // on each keypress, search through groups for textbox input
+    // on each keypress, search through control names for textbox input
     searchBar.keyup(function(){
         var searchBarInput = searchBar.val().toLowerCase();
 
-        // get the immediate child <li> (group element)
-        controlsListElem.children('div').children('div').each(function(index, elem) {
+        // get the immediate child '.formfieldlist > .fd-formfieldlist-item' (control element)
+        controlsListItemElem.children('div').children('div').each(function(index, elem) {
+            // get the current element's text html ('.fd-formfieldlist-item-text > .fd-formfieldlist-text')
             var controlElement = $(elem).children('div').children('span')[0];
-            // if searchbar text is blank OR is included in group name/items
+            // check if searchbar text is blank OR is included in control name
             if (searchBarInput === '' || (controlElement.outerText).toLowerCase().includes(searchBarInput)) {
+                //Search input is blank or control name contains search input text. 
                 $(controlElement).parent().parent().show();
             } else {
+                //Search input is not blank and control name does not contain search input text.
                 $(controlElement).parent().parent().hide();
             }
         });
@@ -34,7 +37,7 @@ var FindControlListHolder = function() {
     // mainContainer holds the most pertinent content of the designer
     var mainContainerMatches = $('.mainContainer-heightControl');
     if (mainContainerMatches.length < 1) {
-        throw new Error('VV5 Groups and Conditions Search - Could not find main container element; HTML structure on document ready has possibly changed. Terminating userscript.');
+        throw new Error('VV5 Controls Search - Could not find main container element; HTML structure on document ready has possibly changed. Terminating userscript.');
     }
     var mainContainer = mainContainerMatches[0];
 
@@ -53,7 +56,7 @@ var FindControlListHolder = function() {
                 controlListElem = tempElem;
             }
         } else if (controlFullyPopulated === false) {
-            // next find the last list item (with class 'k-last') using the grouplist elem as a key
+            // next find the last control element item (with class '.fd-formfieldlist-item') using the controlList elem as a key
             tempElem = controlListElem.find('.fd-formfieldlist-item').last();
             if (tempElem.length > 0) {
                 controlFullyPopulated = true;
@@ -79,4 +82,3 @@ var FindControlListHolder = function() {
 
 // runs function on document ready
 $(document).ready(FindControlListHolder);
-
